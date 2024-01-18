fa5e0c14e51935604039a9d146d1bf7cba864743
Bojan <bojan.adamovic@madduckcode.com>
2024-01-18 15:31:48 +0100

    Implemented HeroBanner

--- ecommercefrontend/app/page.js
+++ ecommercefrontend/app/page.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Home = () => {
+import { client } from "@/lib/client";
+import { Product, FooterBanner, HeroBanner } from "@/components";
+
+const Home = async () => {
+  const {products, bannerData} = await getBannerData();
+
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className="products-heading">
         <h2>Best Selling Products</h2>
@@ -11,12 +16,22 @@ const Home = () => {
       </div>
 
       <div className="product-container">
-        {["Product 1", "Product2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
 
-      Footer
+      <FooterBanner />
     </>
   );
 };
+
+async function getBannerData() {
+  const query = "*[_type == 'product']";
+  const products = await client.fetch(query);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {products, bannerData};
+}
 
 export default Home;
