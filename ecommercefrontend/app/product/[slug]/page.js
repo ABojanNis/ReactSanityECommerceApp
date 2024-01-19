03e7d3e1ec84fcdf7216ac205afca38323dac661
Bojan <bojan.adamovic@madduckcode.com>
2024-01-19 16:19:03 +0100

    Implemented product details page

---
+++ ecommercefrontend/app/product/[slug]/page.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from "react-icons/ai";
+
+import { client, urlFor } from "@/lib/client";
+import { notFound } from "next/navigation";
+import { Product, ProductImages } from "@/components";
+
+const ProductDetails = async ({ params: { slug } }) => {
+  const currentProductQuery = `*[_type == "product" && slug.current == "${slug}"][0]`;
+  const allProductsQuery ="*[_type == 'product']";
+
+  const product = await client.fetch(currentProductQuery);
+  const products = await client.fetch(allProductsQuery);
+
+  if (!product) notFound();
+
+  const { image, name, details, price } = product;
+
+  const images = image.map((image) => ([urlFor(image).url()]));
+
+  return (
+    <div>
+      <div className="product-detail-container">
+        <ProductImages images={images} />
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>
+              (20)
+            </p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+                <span className="minus">
+                  <AiOutlineMinus/>
+                </span>
+                <span className="num">0</span>
+              <span className="plus">
+                  <AiOutlinePlus/>
+                </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart">Add to Cart</button>
+            <button type="button" className="buy-now">Buy Now</button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export async function generateStaticParams() {
+  const query = "*[_type == 'product']";
+  const products = await client.fetch(query);
+
+  return products.map((product) => ({
+    slug: product.slug.current
+  }))
+}
+
+export default ProductDetails;
---
+++ ecommercefrontend/components/ProductImages.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import React, { useState } from 'react';
+
+const ProductImages = ({ images }) => {
+  const [index, setIndex] = useState(0);
+
+  return (
+    <div>
+      <div className="image-container">
+        <img src={images[index]} alt={`image-${index}`} className="product-detail-image"/>
+      </div>
+      <div className="small-images-container">
+        {images?.map((item, i) => (
+          <img
+            src={item}
+            alt={`item-${i}`}
+            key={i}
+            className={i === index ? "small-image selected-image" : "small-image"}
+            onMouseEnter={() => setIndex(i)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductImages;
